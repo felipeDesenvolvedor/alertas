dea19e4b51a9f4770c0e797f02c5c06ca23ea62f
Bernardo Marques <bernardohmarques@gmail.com>
2021-05-12 15:49:50 -0300

    criado footer e adicionado fontes

--- index.php
+++ index.php
@@ -6,6 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="./css/index.css" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel ="stylesheet"/>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,600;1,300&display=swap" rel="stylesheet">
     <script src="./js/index.js"></script>
 
 </head>
@@ -17,5 +23,31 @@
     echo $_GET['q']
 
     ?>
+
+
+    <footer>
+    <div class="container">
+        <div class="row">
+          <div class="col-md-8 offset-md-2 col-sm-12 offset-md-0">
+            <h5>
+              COPYRIGHT © 2021 CIGAM GESTOR - TODOS OS DIREITOS RESERVADOS
+              <br />
+              TELEFONE: (53) 3260-1350 E-MAIL: SUPORTE@CIGAMGESTOR.COM.BR
+            </h5>
+              <div class="social">
+                <a href="https://pt-br.facebook.com/erpcigam/">
+                    <i class="fab fa-facebook-square"></i>
+                </a>
+                <a href="https://www.instagram.com/erpcigam/?hl=pt-br">
+                    <i class="fab fa-instagram"></i>
+                </a>
+                <a href="https://twitter.com/erpcigam">
+                     <i class="fab fa-twitter"></i>
+                </a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </footer>
 </body>
 </html>
